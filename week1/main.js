9c605b83733180f4d1fff1abdc0ae40d8efe982e
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-14 02:10:47 +0900

    [feat] 추가/삭제/수량 변경 시 누적금액 변동

--- week1/main.js
+++ week1/main.js
@@ -1,12 +1,37 @@
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 const burgersInCart = [];
-let totalPrice = 0;
+
+const parsePriceToNumber = (price) => {
+  const removedComma = price.slice(0, -1).replace(/\D/g, "");
+
+  return +removedComma;
+};
 
 // -------------------------------------------------------
 
+function calculateTotalPrice() {
+  let totalPrice = 0;
+  const cartLists = $$(".main__cart-list");
+
+  cartLists.forEach((cartList) => {
+    const currentPrice = parsePriceToNumber(
+      cartList.querySelector("p.main__cart-list-price").innerText
+    );
+    const currentAmount = cartList.querySelector(
+      "input.main__cart-list-amount"
+    ).value;
+    totalPrice += currentPrice * currentAmount;
+  });
+
+  $(".main__cart-total-price").innerText = `${totalPrice}원`;
+}
+
 function deleteBurgerFromCart(e) {
   e.currentTarget.closest("li").remove();
+
+  calculateTotalPrice();
 }
 
 function addNewBurgerToCart(targetBurger, targetBurgerName) {
@@ -14,13 +39,17 @@ function addNewBurgerToCart(targetBurger, targetBurgerName) {
     targetBurger.querySelector("p.main__list-price").innerText;
 
   const cartList = document.createElement("li");
+  cartList.classList.add("main__cart-list");
   cartList.innerHTML = `
     <strong>${targetBurgerName}</strong>
-    <input type="number" value="1" class="main__${targetBurgerName}-amount" />
-    <p>${targetPrice}</p>
+    <input type="number" value="1" min="1" class="main__cart-list-amount main__${targetBurgerName}-amount" />
+    <p class="main__cart-list-price">${targetPrice}</p>
     <button type="button" class="main__cart-list-delete-button">❌</button>
   `;
   $(".main__cart-list-wrapper").appendChild(cartList);
+  cartList
+    .querySelector(`.main__${targetBurgerName}-amount`)
+    .addEventListener("change", calculateTotalPrice);
   cartList
     .querySelector("button.main__cart-list-delete-button")
     .addEventListener("click", (e) => deleteBurgerFromCart(e));
@@ -42,12 +71,14 @@ function addBurgerToCart(e) {
   } else {
     addExistBurgerToCart(targetBurgerName);
   }
+
+  calculateTotalPrice();
 }
 
 function attachClickEventToBurgerBox() {
-  const burgerArticleBoxes = document.querySelectorAll(".main__list");
+  const burgerArticleBoxes = $$(".main__list");
 
-  burgerArticleBoxes.forEach((burgerArticleBox, idx) =>
+  burgerArticleBoxes.forEach((burgerArticleBox) =>
     burgerArticleBox.addEventListener("click", (e) => addBurgerToCart(e))
   );
 }
