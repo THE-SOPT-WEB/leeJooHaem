737d0f73af16dbc0ac69030d3544012f29df0859
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-27 21:07:39 +0900

    [feat] add nations' image files

--- week3/src/pages/Main.tsx
+++ week3/src/pages/Main.tsx
@@ -1,5 +1,28 @@
 import MainView from "components/MainView";
 
 export default function Main() {
+  const nations = [
+    {
+      image: "/assets/korea.png",
+      alt: "대한민국",
+    },
+    {
+      image: "/assets/france.png",
+      alt: "프랑스",
+    },
+    {
+      image: "/assets/england.png",
+      alt: "잉글랜드",
+    },
+    {
+      image: "/assets/netherlands.png",
+      alt: "네덜란드",
+    },
+  ];
+
+  const mainViewProps = {
+    nations,
+  };
+
   return <MainView />;
 }
