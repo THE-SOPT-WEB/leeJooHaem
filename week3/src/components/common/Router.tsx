19d2d2776c8e0b2fbb7992f8a4a0d43dc3e9fd96
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-04-27 13:21:40 +0900

    [feat] setting PreMain page

--- week3/src/components/common/Router.tsx
+++ week3/src/components/common/Router.tsx
@@ -1,12 +1,14 @@
 import Error404 from "pages/Error404";
 import Main from "pages/Main";
+import PreMain from "pages/PreMain";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<PreMain />} />
+        <Route path="/king" element={<Main />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
--- week3/src/pages/PreMain.tsx
+++ week3/src/pages/PreMain.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export default function MainView() {
+export default function PreMain() {
   return (
     <MainWrapper>
       <MainHeader>asd</MainHeader>
