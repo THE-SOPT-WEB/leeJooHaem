7ecee7de13d9893a8b452d6399493c975975cbac
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-10 15:59:05 +0900

    [feat] main container ui

--- week4/src/pages/Main.tsx
+++ week4/src/pages/Main.tsx
@@ -6,11 +6,31 @@ import styled from "styled-components";
 export default function Main() {
   return (
     <StMainContainer>
-      <HeaderSection />
-      <SearchSection />
-      <ListSection />
+      <StMainWrapper>
+        <HeaderSection />
+        <SearchSection />
+        <ListSection />
+      </StMainWrapper>
     </StMainContainer>
   );
 }
 
-const StMainContainer = styled.main``;
+const StMainContainer = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: #565456;
+`;
+
+const StMainWrapper = styled.main`
+  width: 90vw;
+  max-width: 35rem;
+  min-height: 90vh;
+
+  border: 0.1rem solid #f5f5f5;
+  border-radius: 1rem;
+`;
