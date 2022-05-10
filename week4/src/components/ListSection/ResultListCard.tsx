90b84e2c57fc1ba297bf90f0e1e88dfea4823587
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-11 01:20:38 +0900

    [feat] mapping result lists

--- week4/src/components/ListSection/ResultListCard.tsx
+++ week4/src/components/ListSection/ResultListCard.tsx
@@ -1,3 +1,11 @@
-export default function ResultListCard() {
+interface ResultListCardProps {
+  name: string;
+  phoneNumber: string;
+  distance: string;
+}
+
+export default function ResultListCard(props: ResultListCardProps) {
+  const { name, phoneNumber, distance } = props;
+
   return <article>ResultListCard</article>;
 }
--- week4/src/components/ListSection/index.tsx
+++ week4/src/components/ListSection/index.tsx
@@ -1,6 +1,8 @@
 import { ResultList } from "core/types";
 import styled from "styled-components";
 
+import ResultListCard from "./ResultListCard";
+
 interface ListSectionProps {
   resultLists: ResultList[];
   isLoading: boolean;
@@ -11,7 +13,18 @@ export default function ListSection(props: ListSectionProps) {
 
   if (isLoading) return <StResultWrapper>로딩 중🌐🦄</StResultWrapper>;
   if (resultLists.length === 0) return <StResultWrapper>결과가 없어요🤔😘</StResultWrapper>;
-  return <StResultWrapper></StResultWrapper>;
+  return (
+    <StResultWrapper>
+      {resultLists.map((resultList) => (
+        <ResultListCard
+          key={resultList.id}
+          name={resultList.place_name}
+          phoneNumber={resultList.phone}
+          distance={`${resultList.distance}미터`}
+        />
+      ))}
+    </StResultWrapper>
+  );
 }
 
 const StResultWrapper = styled.section`
