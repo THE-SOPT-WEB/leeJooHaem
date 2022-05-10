93d22cdf22926e7d693575879dff4080e5131110
SNUPI <47105088+joohaem@users.noreply.github.com>
2022-05-10 16:06:48 +0900

    [feat] header section ui

--- week4/src/components/HeaderSection/index.tsx
+++ week4/src/components/HeaderSection/index.tsx
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+
 export default function HeaderSection() {
-  return <div>HeaderSection</div>;
+  return <StHeaderSection>동네 킹주집 함가보자</StHeaderSection>;
 }
+
+const StHeaderSection = styled.section`
+  height: 4rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-bottom: 0.1rem solid #f5f5f5;
+
+  color: #c0c46a;
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
