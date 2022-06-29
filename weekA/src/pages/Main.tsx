dc2a41b4848778a6839069c719984d25665789d1
SNUPI <joohaem@daum.net>
2022-06-29 14:02:16 +0900

    [feat] react-slick

--- weekA/src/pages/Main.tsx
+++ weekA/src/pages/Main.tsx
@@ -1,16 +1,51 @@
-import { Swiper, SwiperSlide } from "swiper/react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import { useEffect } from "react";
+import Slider from "react-slick";
+import styled from "styled-components";
+
+const settings = {
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
 
 export default function Main() {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}>
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-    </Swiper>
+    <S.Container>
+      <Slider {...settings}>
+        <div>
+          <S.Item>Slide 1</S.Item>
+        </div>
+        <div>
+          <S.Item onClick={() => window.alert("herre")}>Slide 2</S.Item>
+        </div>
+        <div>
+          <S.Item>Slide 3</S.Item>
+        </div>
+        <div>
+          <S.Item>Slide 4</S.Item>
+        </div>
+      </Slider>
+    </S.Container>
   );
 }
+
+const S = {
+  Container: styled.main`
+    width: 400px;
+    margin: 100px auto;
+
+    background-color: #eeeeee;
+  `,
+  Item: styled.h3`
+    width: 200px;
+    height: 300px;
+
+    background-color: green;
+
+    margin: auto;
+  `,
+};
